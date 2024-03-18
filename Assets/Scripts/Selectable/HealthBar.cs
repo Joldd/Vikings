4e78121a93db0714f1abf5882e58ab8d66120db3
julien <julien.coutouly@gmail.com>
2024-03-18 16:58:01 +0100

    try health bar camera

--- Assets/Scripts/Selectable/HealthBar.cs
+++ Assets/Scripts/Selectable/HealthBar.cs
@@ -3,11 +3,14 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    private GameObject canvas;
     public Slider slider;
     float maxPV;
 
     private void Start()
     {
+        canvas = transform.Find("HUD").gameObject;
+        canvas.SetActive(true);
         slider = transform.Find("HUD").Find("HealthBar").GetComponent<Slider>();
         maxPV = GetComponent<Selectable>().PV;
     }
@@ -15,5 +18,10 @@ public class HealthBar : MonoBehaviour
     public void updateHealthBar()
     {
         slider.value = GetComponent<Selectable>().PV / maxPV;
+    }
+
+    private void Update()
+    {
+        canvas.transform.LookAt(Camera.main.transform);
     }
 }
