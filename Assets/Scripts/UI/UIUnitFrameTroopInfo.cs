091f3af5c782e49cd588b0e8aeb1deec1250ca45
Mornet Valentin <valentin.mornet@gamabilis.com>
2024-06-19 15:56:46 +0200

    Update Troop Info

--- Assets/Scripts/UI/UIUnitFrameTroopInfo.cs
+++ Assets/Scripts/UI/UIUnitFrameTroopInfo.cs
@@ -5,19 +5,26 @@ public class UIUnitFrameTroopInfo : MonoBehaviour
 {
     [SerializeField] private Slider slider;
     [SerializeField] private Image image;
+    [SerializeField] private Image fillImage;
+
+    [SerializeField] private Color killedColor;
+    [SerializeField] private Color aliveColor;
     
+
     //TODO Faire une ecoute d'un event quand l'unit prend des dégats 
 
 
     public void SetupUnitData(EntityUnit unit)
     {
         image.sprite = unit.imageUnit;
-        slider.value = (float) unit.PV/unit.maxPV;
+        UpdateSliderValue((float) unit.PV/unit.maxPV);
         unit.AddListenerOnDamageTaken(UpdateSliderValue);
     }
     
     private void UpdateSliderValue(float value)
     {
         slider.value = value;
+        image.color = value > 0 ? aliveColor : killedColor;
+        fillImage.enabled = value > 0;
     }
 }
